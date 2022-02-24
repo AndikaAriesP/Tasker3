9037130587d3fbc4e44a8ce97a4729dc5d5c3c33
Andika <Andika@LAPTOP-PNGP4BPM>
2022-02-24 20:21:33 +0700

    Add project files.

---
+++ Tasker3/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using RabbitMQ;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Tasker3
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void PageLoadConnectRMQ(FrameworkElement sender, object args)
+        {
+            rmq.InitRMQConnection();
+            rmq.CreateRMQConnection();
+            while (!rmq.connection.IsOpen) { /*loooping hingga koneksi terbuka*/ }
+            rmq.CreateRMQChannel("workQ");
+            while (!rmq.channel.IsOpen) { /*loooping hingga channel terbuka*/ }
+            if (rmq.connection.IsOpen && rmq.channel.IsOpen)
+            {
+                button.IsEnabled = true;
+                Button1.IsEnabled = true;
+            }
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            rmq.Disconnect();
+            button.IsEnabled = false;
+            Button1.IsEnabled = false;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            rmq.SendMessage(Tujuan.Text, Pesan.Text);
+        }
+    }
+}
---
+++ Tasker3/RMQ.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasker3
+{
+    class RMQ
+    {
+        public RabbitMQ.Client.ConnectionFactory connectionFactory;
+        public RabbitMQ.Client.IConnection connection;
+        public RabbitMQ.Client.IModel channel;
+        public void InitRMQConnection(string host = "localhost", int port = 5672, string user = "guest",
+        string pass = "guest")
+        {
+            connectionFactory = new RabbitMQ.Client.ConnectionFactory();
+            connectionFactory.HostName = host;
+            //connectionFactory.Port = port;
+            connectionFactory.UserName = user;
+            connectionFactory.Password = pass;
+        }
+        public void CreateRMQConnection()
+        {
+            connection = connectionFactory.CreateConnection();
+            Console.WriteLine("Koneksi " + (connection.IsOpen ? "Berhasil!" : "Gagal!"));
+        }
+        public void CreateRMQChannel(string queue_name, string routingKey = "", string exchange_name =
+        "")
+        {
+            if (connection.IsOpen)
+            {
+                channel = connection.CreateModel();
+                Console.WriteLine("Channel " + (channel.IsOpen ? "Berhasil!" : "Gagal!"));
+            }
+            if (channel.IsOpen)
+            {
+                channel.QueueDeclare(queue: queue_name,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+                Console.WriteLine("Queue telah dideklarasikan..");
+            }
+        }
+        public void SendMessage(string tujuan, string msg = "send")
+        {
+            byte[] responseBytes = Encoding.UTF8.GetBytes(msg);// konversi pesan dalam bentuk string menjadi byte
+            channel.BasicPublish(exchange: "",
+            routingKey: tujuan,
+            basicProperties: null,
+            body: responseBytes);
+            Console.WriteLine("Pesan: '" + msg + "' telah dikirim.");
+        }
+        public void Disconnect()
+        {
+            channel.Close();
+            channel = null;
+            Console.WriteLine("Channel ditutup!");
+            if (connection.IsOpen)
+            {
+                connection.Close();
+            }
+            Console.WriteLine("Koneksi diputus!");
+            connection.Dispose();
+            connection = null;
+        }
+    }
+
+}
